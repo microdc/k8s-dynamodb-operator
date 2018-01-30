e6dc36db3e59a586ea8f36864d56eb81aca42b20
Tom van Neerijnen <tom@tomvn.com>
2018-01-30 15:26:41 +0000

    Rename k8s CRD group from microdc to microdc.io

--- startup.sh
+++ startup.sh
@@ -4,4 +4,4 @@ set -eo pipefail
 
 kubectl apply -f crd.yaml
 
-exec side8-k8s-operator --resource dynamodbs --fqdn aws.microdc --version v1
+exec side8-k8s-operator --resource dynamodbs --fqdn aws.microdc.io --version v1
